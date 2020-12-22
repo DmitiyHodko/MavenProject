f9b9fb92c52f68be992ae0024dd6e1c665059621
Dmitriy Hod'ko <abcddrrokerrddabc@mail.ru>
2020-12-22 19:56:00 +0300

    Adding maven app

---
+++ src/main/java/test/Servlet.java
@@ -0,0 +1,21 @@
+package test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset = UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        req.setAttribute("hello", "Hello centOS");
+        getServletContext().getRequestDispatcher("/jsp/hello.jsp").forward(req, resp);
+
+    }
+
+}
